7730aa9103aea423d99f283dfa00af1dc55d4094
Kris Meister <kris@aliencom.net>
2024-10-04 11:11:17 -0700

    Moved Pages into subdirecty

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,16 @@
-import {  BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import SimpleState from "../pages/SimpleState";
-import SimpleComponent from "../pages/SimpleComponent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import SimpleState from "../pages/SimpleState/SimpleState";
+import SimpleComponent from "../pages/SimpleComponent/SimpleComponent";
 
 const AppRouter = () => {
-return <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/component-1" element={<SimpleComponent />} />
-            <Route path="/state-1" element={<SimpleState />} />
-      </Routes>
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/component-1" element={<SimpleComponent />} />
+      <Route path="/state-1" element={<SimpleState />} />
+    </Routes>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
